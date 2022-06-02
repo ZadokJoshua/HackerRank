6b9bfd0f1fa5f3bbedd1db00548de63d76c824bd
Zadok Joshua <65626254+ZadokJoshua@users.noreply.github.com>
2022-06-02 11:17:22 +0100

    Update Mini-Max-Sum.cs

--- Mini-Max-Sum.cs
+++ Mini-Max-Sum.cs
@@ -1,17 +1,19 @@
 public static void miniMaxSum(List<int> arr)
 {
-    int arrLength = arr.Count;
-    int originalSum = arr.Sum();
-    List<int> sumsStorage = new List<int>();
+    // Converted the list<int> to list<long> using convert all
+    List<long> longs = arr.ConvertAll(i => (long)i);
+    long arrLength = longs.Count;
+    long originalSum = longs.Sum();
+    List<long> sumsStorage = new List<long>();
 
     for(int i = 0; i < arrLength; i ++)
     {
-        int tempSum = originalSum;
-        tempSum -= arr[i];
+        long tempSum = originalSum;
+        tempSum -= longs[i];
         sumsStorage.Add(tempSum);
     }
 
-    int min = sumsStorage.Min();
-    int max = sumsStorage.Max();
+    long min = sumsStorage.Min();
+    long max = sumsStorage.Max();
     Console.WriteLine(min + " " + max);
 }
